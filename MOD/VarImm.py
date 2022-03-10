2365ea9cd2fd32e574036b26cb9aae82808a0b50
Andre Schlegel <hiroandre@gmx.de>
2022-03-10 10:02:34 +0100

    Andre: Neuer Modular Test.

---
+++ MOD/VarImm.py
@@ -0,0 +1,17 @@
+a = 96
+b = -96
+c = 19
+d = -19
+e = 0
+print(a % 19) # 1
+print(96 % c) # 1
+print(0 % c) # 0
+print(e % 19) # 0
+print(b % 19) # -1
+print(-96 % c) # -1
+print(a % -19) # 1
+print(96 % d) # 1
+print(b % -19) # -1
+print(-96 % d) # -1
+print(0 % d) # 0
+print(e % -19) # 0
